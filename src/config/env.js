655fd8f2f528e9e0fa5058300b26ff30d66d542d
frahim arif <frahim9900@gmail.com>
2026-04-10 21:00:50 +0530

    chang

--- src/config/env.js
+++ src/config/env.js
@@ -11,7 +11,7 @@ export const env = {
   port: Number(process.env.PORT || 5000),
 
   // FRONTEND URL
-  frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5173',
+  frontendUrl: process.env.FRONTEND_URL || 'https://jobhir.com',
 
   // MONGO DB
  mongoUri:
