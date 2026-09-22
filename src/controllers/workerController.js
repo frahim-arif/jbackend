3f4896dc481e47fa891f9e35e77acc91119ed495
frahim arif <frahim9900@gmail.com>
2026-09-22 10:28:25 +0530

    gfd

--- src/controllers/workerController.js
+++ src/controllers/workerController.js
@@ -5,7 +5,7 @@ import { Worker } from "../models/Worker.js";
 // WORKER REGISTRATION PAYMENT
 // =====================================================
 
-const WORKER_REGISTRATION_AMOUNT = 500; // ₹250 in paise
+const WORKER_REGISTRATION_AMOUNT = 25000; // ₹250 in paise
 
 // =====================================================
 // REGISTER WORKER
